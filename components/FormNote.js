2cb9c522cbb7993a1cb728dc0db3f582601e8b15
Freddy Munive <munivef@yahoo.com>
2020-06-19 11:13:57 -0500

    join card, colors and main html

--- components/FormNote.js
+++ components/FormNote.js
@@ -1,72 +1,50 @@
-const template_form = document.createElement('template');
+const template_form = document.createElement("template");
 
 template_form.innerHTML = `
-  <style>
-    .form-note {
-      height: 130px;
-      width: 80%;
-      max-width: 600px;
-      padding: 20px;
-      margin: 0 auto;
-      box-shadow: 5px 5px 15px rgba(153, 155, 158, 0.85);
-      border-radius: 8px;
-      color: #000000;
-    }
-    .form-content {
-      height: calc(100% - 38px);
-    }
-    .form-control {
-      height: 38px;
-      display: flex;
-      justify-content: space-between;
-    }
-    .form-body {
-      width: 100%;
-      resize: none;
-      border: 0px;
-      font-family: Roboto;
-      font-style: normal;
-      font-weight: normal;
-      color: black;
-    }
-
-    .form-white {
-      background-color: white;
-    }
-
-    .note-footer-icon {
-      display: inline-block;
-      height: 36px;
-      width: 36px;
-      background-size: cover;
-    }
-    .footer-icon-color {
-      background-image: url(../img/icon_without_hover.svg);
-    }
-    .footer-icon-color:hover {
-      background-image: url(../img/icon_with_hover.svg);
-    }
-
-    .form-control__keep-it {
-      font-weight: bold;
-      font-size: 18px;
-      line-height: 24px;
-    }
-    .form-btn  {
-      height: 36px;
-      background-color: transparent;
-      border: none;
-    }
-  </style>
-
+  <link rel="stylesheet" href="./css/color_container.css" />
+  <link rel="stylesheet" href="./css/note.css" />
   <div class="form-note form-white">
     <div class="form-content">
       <textarea name="body" id="body" cols="30" rows="5" class="form-body" placeholder="Some great think!"></textarea>
     </div>
     <div class="form-control">
-      <button class="form-btn form-control__color">
-        <span class="note-footer-icon footer-icon-color"></span>
-      </button>
+        <div class="note-footer">
+        <div class="note-footer-icon-color">
+          <span class="note-footer-icon footer-icon-color"></span>
+          <div class="icon-color-dropdown-content">
+            <button
+              class="color-dropdown-content-circulo color-FFFFFF"
+            ></button>
+            <button
+              class="color-dropdown-content-circulo color-F28B82"
+            ></button>
+            <button
+              class="color-dropdown-content-circulo color-FBBC04"
+            ></button>
+            <button
+              class="color-dropdown-content-circulo color-FFF475"
+            ></button>
+            <button
+              class="color-dropdown-content-circulo color-CCFF90"
+            ></button>
+            <button
+              class="color-dropdown-content-circulo color-A7FFEB"
+            ></button>
+            <button
+              class="color-dropdown-content-circulo color-CBF0F8"
+            ></button>
+            <button
+              class="color-dropdown-content-circulo color-AECBFA"
+            ></button>
+            <button
+              class="color-dropdown-content-circulo color-D7AEFB"
+            ></button>
+            <button
+              class="color-dropdown-content-circulo color-FDCFE8"
+            ></button>
+          </div>
+        </div>
+      </div>
       <button class="form-btn form-control__keep-it">
         Keep it!
       </button>
@@ -78,21 +56,21 @@ class FormNote extends HTMLElement {
   constructor() {
     super();
 
-    this.attachShadow({mode: 'open'});
+    this.attachShadow({ mode: "open" });
     this.shadowRoot.appendChild(template_form.content.cloneNode(true));
   }
 
   connectedCallback() {
-    console.log('connectedCallback');
-  };
+    console.log("connectedCallback");
+  }
 
   disconnectedCallback() {
-    console.log('disconnectedCallback');
+    console.log("disconnectedCallback");
   }
 
   attributeChangedCallback() {
-    console.log('attributeChangedCallback');
+    console.log("attributeChangedCallback");
   }
 }
 
-window.customElements.define('form-note', FormNote);
+window.customElements.define("form-note", FormNote);
--- components/NoteCard.js
+++ components/NoteCard.js
@@ -1,87 +1,80 @@
-const template_note_card = document.createElement('template');
+const template_note_card = document.createElement("template");
 template_note_card.innerHTML = `
-<style>
-  .card {
-    height: 200px;
-    width: 200px;
-    padding: 20px;
-    box-shadow: 5px 5px 15px rgba(153, 155, 158, 0.85);
-    border-radius: 8px;
-    color: #000000;
-  }
-  .card-content {
-    font-size: 13px;
-    line-height: 24px;
-    min-height: calc(100% - 38px);
-  }
-  .card-options {
-    height: 38px;
-  }
-
-  .card-white {
-    background-color: white;
-  }
-
-  .note-footer-icon {
-    display: inline-block;
-    width: 36px;
-    height: 36px;
-    background-size: cover;
-  }
-  .footer-icon-color {
-    background-image: url(../img/icon_without_hover.svg);
-  }
-  .footer-icon-color:hover {
-    background-image: url(../img/icon_with_hover.svg);
-  }
-
-  .footer-icon-trash {
-    background-image: url(../img/trash.svg);
-  }
-  .footer-icon-trash:hover {
-    background-image: url(../img/trash_with_hover.svg);
-  }
-
-  button {
-    background-color: transparent;
-    border: none;
-  }
-</style>
-
+<link rel="stylesheet" href="./css/color_container.css" />
+<link rel="stylesheet" href="./css/note.css" />
 <div class="card">
   <div class="card-content">
     <p class="card-title"></p>
   </div>
   <div class="card-options" >
-    <button class="card-options__color">
+    <div class="note-footer">
+    <div class="note-footer-icon-color">
       <span class="note-footer-icon footer-icon-color"></span>
-    </button>
-    <button class="card-options__trash">
-      <span class="note-footer-icon footer-icon-trash"></span>
-    </button>
+      <div class="icon-color-dropdown-content">
+        <button
+          class="color-dropdown-content-circulo color-FFFFFF"
+        ></button>
+        <button
+          class="color-dropdown-content-circulo color-F28B82"
+        ></button>
+        <button
+          class="color-dropdown-content-circulo color-FBBC04"
+        ></button>
+        <button
+          class="color-dropdown-content-circulo color-FFF475"
+        ></button>
+        <button
+          class="color-dropdown-content-circulo color-CCFF90"
+        ></button>
+        <button
+          class="color-dropdown-content-circulo color-A7FFEB"
+        ></button>
+        <button
+          class="color-dropdown-content-circulo color-CBF0F8"
+        ></button>
+        <button
+          class="color-dropdown-content-circulo color-AECBFA"
+        ></button>
+        <button
+          class="color-dropdown-content-circulo color-D7AEFB"
+        ></button>
+        <button
+          class="color-dropdown-content-circulo color-FDCFE8"
+        ></button>
+      </div>
+    </div>
+    <span class="note-footer-icon footer-icon-trash"></span>
+  </div>
   </div>
 </div>
-`
+`;
 
 class NoteCard extends HTMLElement {
   constructor() {
     super();
 
-    this.attachShadow({mode: 'open'});
+    this.attachShadow({ mode: "open" });
     this.shadowRoot.appendChild(template_note_card.content.cloneNode(true));
-    this.shadowRoot.querySelector('.card-title').innerText = this.getAttribute('body');
-    this.shadowRoot.querySelector('.card').classList.add("card-white");
+    this.shadowRoot.querySelector(".card-title").innerText = this.getAttribute(
+      "body"
+    );
+    this.shadowRoot.querySelector(".card").classList.add("card-white");
   }
 
   connectedCallback() {
-    this.addEventListener('click', e => {
+    this.addEventListener("click", (e) => {
       this.toggleCard();
+      console.log(e.target);
     });
   }
 
   toggleCard() {
-    console.log('click')
+    console.log("click");
+  }
+
+  colorNote() {
+    console.log("note card", this);
   }
 }
 
-window.customElements.define('note-card', NoteCard);
+window.customElements.define("note-card", NoteCard);
