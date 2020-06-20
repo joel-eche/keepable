9ac44d62199ba00e1d0c434d32c1d3eb9c3169df
Freddy Munive <munivef@yahoo.com>
2020-06-19 19:50:59 -0500

    adding responsive list

--- components/ListNotes.js
+++ components/ListNotes.js
@@ -10,10 +10,29 @@ const templateNotes = `
   <style>
     #list-notes {
       display: grid;
-      grid-gap: 10px;
-      grid-template-columns: repeat(auto-fill, 24%);
+      grid-gap: 1em;
+      grid-template-columns: 1fr;
+      justify-items:center;
       width: 100%;
       margin-top: 60px;
+    }
+
+    @media (min-width: 568px){
+      #list-notes {
+        grid-template-columns: 1fr 1fr;
+      }
+    }
+
+    @media (min-width: 768px){
+      #list-notes {
+        grid-template-columns: 1fr 1fr 1fr;
+      }
+    }
+
+    @media (min-width: 992px){
+      #list-notes {
+        grid-template-columns: repeat(4, 1fr);
+      }
     }
   </style>
 
@@ -47,20 +66,23 @@ class ListNotes extends HTMLElement {
   filterNotes() {
     let notes = JSON.parse(localStorage.getItem("notes"));
     if (this.getAttribute("filter") === "active") {
-      return notes.filter((note) => note.active === true );
+      return notes.filter((note) => note.active === true);
     }
-    return notes.filter((note) => note.active === false );
+    return notes.filter((note) => note.active === false);
   }
 
   render() {
     let notes = this.filterNotes();
-    
+
     notes.forEach((note) => {
       let note_card = document.createElement("note-card");
       note_card.setAttribute("id", note.id);
       note_card.setAttribute("body", note.body);
       note_card.setAttribute("class-color", note.classColor);
-      note_card.setAttribute("active", note.active ? "active-note" : "inactive-note");
+      note_card.setAttribute(
+        "active",
+        note.active ? "active-note" : "inactive-note"
+      );
       this.shadowRoot.getElementById("list-notes").appendChild(note_card);
     });
   }
